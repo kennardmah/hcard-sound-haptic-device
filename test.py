a3533071c83e03b0e6559b2e200424e25fb51ec0
Ken <73114932+kennardmah@users.noreply.github.com>
2024-02-20 13:23:05 +0000

    Update

--- test.py
+++ test.py
@@ -5,6 +5,4 @@ p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     dev = p.get_device_info_by_index(i)
     print(f"Device {i}: {dev['name']}, Channels: {dev['maxInputChannels']}")
-
-
     

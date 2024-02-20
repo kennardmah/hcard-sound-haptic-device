1abbb3eadcd13b75ce2a18c26c620485787e3a26
Ken <73114932+kennardmah@users.noreply.github.com>
2024-02-20 16:44:56 +0000

    added arduino stuff

---
+++ Arduino/arduino_device/arduino_device.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // write code
+  Serial.begin(9600);
+}
+
+void loop() {
+  // write code
+  if (Serial.available() > 0) {
+    // Read the incoming data as a string
+    String data = Serial.readStringUntil('\n');
+    // Debug print the received string
+    Serial.print("Received: ");
+    Serial.println(data);
+  }
+}

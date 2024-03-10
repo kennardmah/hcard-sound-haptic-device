4626bbe9c07e72d014422340b54613f379b24b1a
Ken <73114932+kennardmah@users.noreply.github.com>
2024-03-10 13:04:58 +0000

    updated

--- Arduino/arduino_device/arduino_device.ino
+++ Arduino/arduino_device/arduino_device.ino
@@ -1,13 +1,18 @@
 void setup() {
   // Start serial communication at 9600 bps.
   Serial.begin(9600);
-//  pinMode(3, OUTPUT);
-//  pinMode(5, OUTPUT);
-//  pinMode(6, OUTPUT);
-//  pinMode(9, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(10, OUTPUT);  
   pinMode(11, OUTPUT);  
 }
+
+//back right 11, 5
+//front right 3 2
+//front 5 3
+//front left 6 1 
+//back left 10 , 4
 
 void loop() {
   if (Serial.available() >= 2) {
@@ -15,12 +20,10 @@ void loop() {
     // Read serial array 
     Serial.readBytes((char*)arr, 5);
     //Control output  in range 0 - 255)
-    analogWrite(10, (arr[0]));
-    analogWrite(11, (arr[1]));
-//    analogWrite(6, (arr[2]);
-//    analogWrite(9, (arr[3]);  
-//    analogWrite(11, (arr[4]);
- //   analogWrite(11, (arr[5]);
- //   analogWrite(, (arr[6]);
+    analogWrite(10, (arr[3]));
+    analogWrite(11, (arr[4]));
+    analogWrite(6, (arr[0]));
+    analogWrite(5, (arr[2]));  
+    analogWrite(3, (arr[1]));
   }
 }  
--- surround_sound.py
+++ surround_sound.py
@@ -68,8 +68,8 @@ RATE = 44100  # Sample rate (samples per second)
 CHUNK = 1024  # Number of frames per buffer
 
 # SERIAL CONNECTION SET-UP
-COM_PORT = '/dev/cu.usbmodem144301'
-# arduino = serial.Serial(COM_PORT, 9600) # COMMENT OUT W/O ARDUINO
+COM_PORT = '/dev/cu.usbmodem144401'
+arduino = serial.Serial(COM_PORT, 9600) # COMMENT OUT W/O ARDUINO
 time.sleep(2) # slight delay for connection
 
 blackhole_index = find_blackhole_device_index(p)
@@ -119,13 +119,13 @@ try:
         # defining and converting intensity into 0, 128, 256
         intensity = amplitude(audio_data)
         intensity = smooth.add_data(intensity)
-        data_normalized = [0 if value < 800 else 128 if value < 2000 else 255 for value in intensity]
+        data_normalized = [0 if value < 6000 else 50 if value < 8000 else 100 for value in intensity]
         cmdArrayFloat = np.array(data_normalized, dtype=np.uint8) # array of 2 uint8
         print(intensity)
         cmd_bytes = cmdArrayFloat.tobytes() # array of 16 bytes
-        testing.append(data_normalized)  # Convert np.array to list for easier handling later
+        testing.append(intensity)  # Convert np.array to list for easier handling later
         # print(cmd_bytes)
-        # n = arduino.write(cmd_bytes)# send the command # COMMENT OUT W/O ARDUINO
+        n = arduino.write(cmd_bytes)# send the command # COMMENT OUT W/O ARDUINO
         # print(f"{n} bytes sent")
 except KeyboardInterrupt:
     print("Stopping audio stream...")
